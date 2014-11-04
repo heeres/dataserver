8bd22079bdd8ccb757478dd21f9be3506c342954
Phil Reinhold <pcreinhold@gmail.com>
2014-11-04 09:47:38 -0500

    helper methods that deal with both local and remote hdf5 files

--- dataserver_helpers.py
+++ dataserver_helpers.py
@@ -1,4 +1,6 @@
 import time
+import objectsharer
+
 DATA_DIRECTORY = r'C:\_Data'
 
 def dataserver_client(serveraddr='127.0.0.1', serverport=55556, localaddr='127.0.0.1'):
@@ -26,7 +28,7 @@ def get_file(filename, groupname="", timestamp_group=False, **kwargs):
     return f
 
 def run_dataserver(qt=False):
-    from dataserver.dataserver import start
+    from dataserver import start
     import os
     os.chdir(DATA_DIRECTORY)
     start(qt)
@@ -37,3 +39,44 @@ def resolve_file(filename, path):
     for group in path.split('/'):
         file = file.get_group(group)
     return file.get_numbered_child()
+
+def get_group(file, groupname):
+    if groupname not in file:
+        file.create_group(groupname)
+    return file[groupname]
+
+
+def resolve_path(file, path):
+    if isinstance(path, str):
+        path = path.split("/")
+    cur = file
+    for p in path:
+        cur = get_group(cur, p)
+    return cur
+
+def get_numbered_child(file):
+    max_n = 0
+    for k in file.keys():
+        try:
+            n = int(k)
+            max_n = max(n, max_n)
+        except ValueError:
+            pass
+    return file.create_group(str(max_n+1))
+
+def set_scale(group, xname, yname, dim=0, label=None):
+    if isinstance(group, objectsharer.ObjectProxy):
+        group.set_scale(xname, yname, dim=dim, label=label)
+    else:
+        if label is None:
+            label = xname
+        group[yname].dims.create_scale(group[xname], label)
+        group[yname].dims[dim].attach_scale(group[xname])
+
+def update_attrs(group, dict):
+    if isinstance(group, objectsharer.ObjectProxy):
+        group.set_attrs(**dict)
+    else:
+        for k in dict:
+            print k, dict[k]
+            group.attrs[k] = dict[k]
